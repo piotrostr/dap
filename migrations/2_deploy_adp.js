3ca433dc6ccda253eb8ba956762e9001475fe96b
Piotr <piotr.ostrowski@aol.com>
2022-01-31 23:17:40 +0100

    migrate to hardhat

--- migrations/2_deploy_adp.js
+++
@@ -1,8 +0,0 @@
-const DAP = artifacts.require('DegenerateApeParty')
-
-const uniswapRouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
-
-module.exports = async (deployer) => {
-  deployer.deploy(DAP, uniswapRouterAddress)
-}
-
